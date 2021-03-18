42882818f0cea5b6cf467e82f19f03f2f521f4c8
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-03-17 20:18:32 -0600

    Added grammar module.

---
+++ python/grammar/grammar.py
@@ -0,0 +1,13 @@
+# grammar: grammar related functions.
+#
+# Copyright (C) 2021, Ty Gillespie. All rights reserved.
+# MIT License.
+
+def plural(number, option1, option2):
+	r"""Returns the plural of the item if the number is more than 1, or less than -1.
+	Takes the choices as parems, doesn't just add "s", so you can avoid "goose" becoming "gooses", for example.
+	"""
+	if number == 1 or number == -1:
+		return option1
+	else:
+		return option2
---
+++ python/grammar/test.py
@@ -0,0 +1,9 @@
+# Tests the grammar module.
+#
+# Copyright (C) 2021, Ty Gillespie. All rights reserved.
+# MIT License.
+
+import grammar
+
+print(grammar.plural(1, "bat", "bats"))
+print(grammar.plural(2, "bat", "bats"))

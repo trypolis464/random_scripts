480fcd3721eef2ad5e0de5d3688c28f0ccca6236
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-03-17 19:36:45 -0600

    Added tests.

---
+++ python/token_generator/test.py
@@ -0,0 +1,9 @@
+# Tests the token_generator module.
+#
+# Copyright (C) 2021, Ty Gillespie. All rights reserved.
+# MIT License.
+
+import token_generator
+
+print(token_generator.generate())
+print(token_generator.generate(12))

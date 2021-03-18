10b3fe24ea7ea5bc552996bc543957c2fb23d442
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-03-17 19:35:36 -0600

    Added first script.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # random_scripts
+
 A collection of random scripts that I write, that aren't big enough to need their own repo.
+
+## Why?
+
+I tend to write small scripts that are certainly useful, but don't really deserve  their own GitHub repo. So, this was born.
+
+## List of scripts.
+* Python
+	* token_generator: generates random strings from a set list of characters.
---
+++ python/token_generator/token_generator.py
@@ -0,0 +1,15 @@
+# token_generator. Generate random strings.
+#
+# Copyright (C) 2021, Ty Gillespie. All rights reserved.
+# MIT License.
+
+import random
+
+def generate(length = 8):
+	"""Generates a token of the given length. The default is 8."""
+	# Feel free to change this based on what you need your tokens to contain.
+	SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+	final = ""
+	for i in range(length):
+		final += SYMBOLS[random.randint(0, SYMBOLS.length() - 1)]
+	return final
